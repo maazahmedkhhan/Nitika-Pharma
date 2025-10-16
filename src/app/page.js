1fc8e941d3cc5902565f50df4db7092fe31374a4
Maaz Ahmed Khan <maazahmedkhhan@gmail.com>
2025-10-16 16:08:14 +0530

    next file delete

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,7 @@ import WhyNikita from './Components/WhyNikita';
 import AboutUs from './Components/AboutUs';
 import Footer from './Components/Footer';
 import GlobalPresence from './Components/GlobalPresence';
-import Header1 from './Components/Header1';
 import Brands from './Components/Brands';
-import FloatingSpeedDial from './Components/SpeedDial';
 import Testamonials from './Components/Testamonials';
 
 const Home = () => {
